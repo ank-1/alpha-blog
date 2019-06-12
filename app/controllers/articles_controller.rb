eca86ec4515fa9696c70b553e72018a6c29e75f6
my rails app <kaushik.dharmesh97@gmail.com>
2019-06-12 22:04:29 +0530

    removed redundancies and make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article,only: [:edit,:update,:destroy,:show]
   def index
     @articles=Article.all
   end
@@ -18,17 +19,14 @@ class ArticlesController < ApplicationController
  end
 
    def show
-     @article=Article.find(params[:id])
    end
 
 
 def edit
-  @article=Article.find(params[:id])
 end
 
 
 def destroy
-  @article=Article.find(params[:id])
   @article.destroy
   flash[:notice]="Article was successfully deleted"
   redirect_to articles_path
@@ -36,7 +34,6 @@ end
 
 
   def update
-    @article=Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article have been successfully updated"
       redirect_to article_path(@article)
@@ -46,10 +43,14 @@ end
   end
 
 
-
+private
 
   def article_params
     params.require(:article).permit(:title,:description)
+  end
+
+  def set_article
+    @article=Article.find(params[:id])
   end
 
 end
