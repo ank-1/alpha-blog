20de1dd7602704316da059f40616c522f052babd
my rails app <kaushik.dharmesh97@gmail.com>
2019-06-12 18:17:41 +0530

    user interface to show,edit and articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles=Article.all
+  end
 
   def new
     @article=Article.new
@@ -6,9 +9,37 @@ class ArticlesController < ApplicationController
 
   def create
     @article=Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "New article has been successfully created"
+      redirect_to article_path(@article)
+   else
+     render 'new'
+   end
+ end
+
+   def show
+     @article=Article.find(params[:id])
+   end
+
+
+def edit
+  @article=Article.find(params[:id])
+end
+
+
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article have been successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
+
+
+
+
   def article_params
     params.require(:article).permit(:title,:description)
   end
